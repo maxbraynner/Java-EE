6c271da02a66fbf4707f934c2a5c8583945f76e1
Max Braynner <max.braynner@gmail.com>
2018-12-15 21:34:47 -0300

    Melhoria na session bean

--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -2,14 +2,17 @@ package br.com.caelum.livraria.dao;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
 public class AutorDao {
 
-	private Banco banco = new Banco();
+	@Inject
+	private Banco banco;
 
 	public void salva(Autor autor) {
 		banco.save(autor);
@@ -22,6 +25,11 @@ public class AutorDao {
 	public Autor buscaPelaId(Integer autorId) {
 		Autor autor = this.banco.buscaPelaId(autorId);
 		return autor;
+	}
+	
+	@PostConstruct
+	private void aposCraicao() {
+		System.out.println("[INFO] AutorDao foi criado.");
 	}
 	
 }
--- livraria/src/br/com/caelum/livraria/dao/Banco.java
+++ livraria/src/br/com/caelum/livraria/dao/Banco.java
@@ -3,10 +3,16 @@ package br.com.caelum.livraria.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 import br.com.caelum.livraria.modelo.Usuario;
 
+@Singleton
+@Startup
 public class Banco {
 	
 	public static List<Livro> livros = new ArrayList<Livro>();
@@ -76,6 +82,11 @@ public class Banco {
 		livros.add(new Livro("JSF Efficaz",coelho));
 		
 		usuarios.add(new Usuario("admin", "pass"));
+	}
+	
+	@PostConstruct
+	private void inicializou() {
+		System.out.println("Banco inicializado");
 	}
 
 }
